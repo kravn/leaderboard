3f31f9fcb0aab4b170042425b08c5030315d515b
Karl Fabe <kmarxfabe@gmail.com>
2018-11-17 21:51:12 +0800

    Adjusted banners

--- components/banner.js
+++ components/banner.js
@@ -9,12 +9,22 @@ Vue.component('banner', {
 	template:
 	`	
 		<div>
+
+			<!--<v-carousel
+			height="300px"
+			hide-delimiters>
+				<v-carousel-item :src="image">
+
+				</v-carousel-item>
+			</v-carousel>-->
+
 			<v-card>
 				<v-img
 				:src="image">
 				</v-img>
 			</v-card>
 			<div class="div-spacer"></div>
+			
 		</div>
 	`	
 })
